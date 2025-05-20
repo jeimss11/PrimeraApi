43c584e2e1a156d521b0d8787addd60070450976
James HS <jeimssbtc@gmail.com>
2025-05-19 22:54:00 -0400

    conexion base de datos

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
+const postRoute= require('./routes/post');
+app.use('/servicios', postRoute);
 
-
-app.get('/', (req, res) => {
-    res.send('servidor funcionando');
-});
+mongoose.connect('mongodb+srv://jeimss11:<ismI3fcLkSe5VnZ3>@senaclouster.yze2nef.mongodb.net/?retryWrites=true&w=majority&appName=SenaClouster', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.listen(10000);
